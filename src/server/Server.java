06695b1c7210895c515640a9eb99408e792d342d
Kostya <KonstantinLikes@gmail.com>
2020-09-12 20:36:03 +1000

    Урок 8

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,14 +1,11 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Vector;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Server {
     private List<ClientHandler> clients;
@@ -18,7 +15,7 @@ public class Server {
     ServerSocket server = null;
     Socket socket = null;
 
-    public Server(){
+    public Server() {
         clients = new Vector<>();
         authService = new SimpleAuthService();
 
@@ -48,19 +45,61 @@ public class Server {
         return authService;
     }
 
-    public void broadcastMsg(ClientHandler sender, String msg){
-        String message = String.format("%s : %s", sender.getNickname(), msg);
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
+
+        String message = String.format(" %s %s : %s", formater.format(new Date()), sender.getNickname(), msg);
         for (ClientHandler c : clients) {
             c.sendMsg(message);
         }
     }
 
-    public void subscribe(ClientHandler clientHandler){
-        clients.add(clientHandler);
+    public void privateMsg(ClientHandler sender, String receiver, String msg) {
+        String message = String.format("[%s] private [%s] : %s", sender.getNickname(), receiver, msg);
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(receiver)) {
+                c.sendMsg(message);
+                if (!c.equals(sender)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+
+        sender.sendMsg("not found user: " + receiver);
     }
 
-    public void unsubscribe(ClientHandler clientHandler){
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastClientList();
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
         clients.remove(clientHandler);
+        broadcastClientList();
+    }
+
+
+    public boolean isLoginAuthenticated(String login) {
+        for (ClientHandler c : clients) {
+            if (c.getLogin().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void broadcastClientList() {
+        StringBuilder sb = new StringBuilder("/clientlist ");
+        for (ClientHandler c : clients) {
+            sb.append(c.getNickname()).append(" ");
+        }
+
+        String msg = sb.toString();
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
     }
 
 }
